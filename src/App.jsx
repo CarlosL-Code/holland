ce8c999a0800e83199079b3a9ff439657ed0415f
Carlos Lozano <carlos@example.com>
2026-09-07 11:53:46 -0300

    Restore contact form and marquee slider

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Colegios from './components/Colegios';
 import Productos from './components/Productos';
+import Contacto from './components/Contacto';
 import Footer from './components/Footer';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Hero />
       <Colegios />
       <Productos />
+      <Contacto />
       <Footer />
     </div>
   );
